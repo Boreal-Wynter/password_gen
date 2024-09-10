5637e9dadcdb3d5632b913f31b984420f121441f
ravine888 <boreal.wynter@gmail.com>
2024-09-09 21:43:45 -0400

    updated the UI of the application

--- generate_password.py
+++ generate_password.py
@@ -35,36 +35,38 @@ def generate_password():
 	# Return password as a string
 	return ''.join(password)
 
-
 # Initialize Tkinter using a Tk root widget
 root = tk.Tk()
 
 # Set application title
 root.title("Password Generator")
 
+# Set application color
+root.configure(bg = '#abc4ff')
+
 # Set fixed application window size
-root.geometry("500x500")								### CHANGE LATER
+root.geometry("250x325")
 root.resizable(False, False)
 
 # Create function to get a new password
 def get_password(widget):
 	# Insert new password on a new line
-    widget.config(state='normal')
-    widget.insert(tk.END, "\n" + generate_password())
-    widget.config(state='disabled')
+    widget.config(state = 'normal')
+    widget.insert(tk.END, generate_password() + "\n")
+    widget.config(state = 'disabled')
+
+    # if the new password goes beyond the text box, scroll down 
     widget.see(tk.END)
 
+# Create widget to insert passwords
+text_widget = tk.Text(root, height = 15, width = 20, padx = 10)
+text_widget.config(bg = '#f5eadd')
+text_widget.pack(pady = 5)
 
-text_widget = tk.Text(root, height=10, width=50)
-text_widget.pack()
-
-text_widget.insert(tk.END, generate_password())
-text_widget.config(state='disabled')
-
-
-button = tk.Button(root, text = "New Password", command = lambda:get_password(text_widget))
-button.pack()
-
+# Create a password that call a function to insert a newly generated password to the text widget
+button = tk.Button(root, pady = 10, text = "New Password", command = lambda:get_password(text_widget))
+button.config(width = root.winfo_screenwidth(), bg = '#809bce')
+button.pack(pady = 15, side = tk.BOTTOM)
 
 # Tkinter event loop
 root.mainloop()
